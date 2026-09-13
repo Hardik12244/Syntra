7317cb7d0b281206ab00419b865dde8dc895ac39
Hardik <hardik12244@gmail.com>
2026-09-14 02:47:25 +0530

    -uplog bug fix

--- backend/src/multer.ts
+++ backend/src/multer.ts
@@ -4,12 +4,18 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+if (process.env.CLOUDINARY_URL) {
+  cloudinary.config({
+    secure: true,
+  });
+} else {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+  });
+}
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
